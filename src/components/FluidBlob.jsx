7579d626ab4d9f955f62f714d44ac4a1846b7459
niranj-r <niranj.njai@gmail.com>
2026-07-03 03:02:16 +0530

    feat: add FluidBlob interactive scene and disable CI build environment for Vercel deployment

--- src/components/FluidBlob.jsx
+++ src/components/FluidBlob.jsx
@@ -1,7 +1,7 @@
 'use client';
 import React, { useMemo, useRef } from 'react';
 import { Canvas, useFrame, extend, useThree } from '@react-three/fiber';
-import { MathUtils, Vector3, Color } from 'three';
+import { MathUtils, Vector3 } from 'three';
 import * as THREE from 'three';
 import { Environment, Lightformer, Text } from '@react-three/drei';
 
